6b522a451e45ba651b7acbc29a2a3eb8dd8948b3
Spusik322 <fpm.vatkin@bsu.by>
2025-03-19 11:07:15 +0300

    только вывод чут чут обновил

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include <iomanip>
 
 int main() {
-    setlocale(LC_ALL, "russian");
     unsigned short A1 = 0x7FFF, A2 = 0x0000;
     unsigned short B1 = 0xABCD, B2 = 0x0000;
-    short A3 = 0x8FFF, B3 = 0x0001, C3;
+    short A3 = 0x7FFF, B3 = 0x0001, C3;
     unsigned short C1, C2;
 
     _asm {
@@ -30,15 +29,15 @@ int main() {
         mov C3, ax;
     }
 
-    std::cout << "В шеснадцатиричной: ";
-    std::cout << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(C3 & 0xFFFF) << " ";
-    std::cout << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(C2 & 0xFFFF) << " ";
-    std::cout << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(C1 & 0xFFFF) << "\n";
+    std::cout << "IN 16: ";
+    std::cout << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(C3) << " ";
+    std::cout << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(C2) << " ";
+    std::cout << std::hex << std::setfill('0') << std::setw(4) << static_cast<int>(C1) << "\n";
 
     long long result = static_cast<long long>(C3);
     result = (result << 16) | C2;
     result = (result << 16) | C1;
 
-    std::cout << "В десятичной: " << std::dec << result << "\n";
+    std::cout << "IN 10: " << std::dec << result << "\n";
     return 0;
 }
